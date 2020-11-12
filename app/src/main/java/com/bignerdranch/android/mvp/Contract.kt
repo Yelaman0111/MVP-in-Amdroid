1b4c8338bd2b767d16c4ed39c053d52661654622
Yelaman <elaman1995.10.01@mail.ru>
2020-11-12 17:43:06 +0600

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/mvp/Contract.kt
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.mvp
+
+interface Contract {
+
+    interface View{
+        fun showProgress()
+
+        fun hideProgress()
+
+        fun setString(string: String?)
+    }
+
+
+    interface Model{
+        interface OnFinishedListener{
+            fun onFinished(string: String?)
+        }
+
+        fun getNextCourse(onFinishedListener: OnFinishedListener?)
+
+    }
+
+
+    interface Presenter{
+        fun onButtonClick()
+
+        fun onDestroy()
+    }
+
+
+}
